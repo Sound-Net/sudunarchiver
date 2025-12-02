b58874df60c4a3765373ca15fa74af4f29102aeb
Jamie Mac <macster110@gmail.com>
2025-12-02 16:21:50 +0000

    Update to new X3 library version

--- sudunarchiver/src/main/java/org/soundnet/sudunarchiver/App.java
+++ sudunarchiver/src/main/java/org/soundnet/sudunarchiver/App.java
@@ -10,7 +10,6 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
